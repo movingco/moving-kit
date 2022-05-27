7190d65ebe5fc493a9a0deecdc5437afa763f8a4
Ian Macalinao <ian@moving.wtf>
2022-05-27 16:18:18 -0500

    re-export math

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,3 +6,23 @@ export * from "./misc";
 export * from "./moveType";
 export * from "./publicKey";
 export * from "./types";
+
+// re-export token-math types
+// so consumers don't need to use them
+export type {
+  BigintIsh,
+  Fraction,
+  IFormatUint,
+  makeDecimalMultiplier,
+  MAX_U64,
+  MAX_U256,
+  NumberFormat,
+  ONE,
+  parseBigintIsh,
+  Percent,
+  Rounding,
+  TEN,
+  validateU64,
+  validateU256,
+  ZERO,
+} from "@ubeswap/token-math";
