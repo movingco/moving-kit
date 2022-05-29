d80fd9ba09e22a5a71910874e8921a2db0795a90
Ian Macalinao <ian@moving.wtf>
2022-05-29 14:33:00 -0500

    option utils

--- packages/core/src/misc.ts
+++ packages/core/src/misc.ts
@@ -1,29 +1,7 @@
 import { MAX_U64, ZERO } from "@ubeswap/token-math";
 import JSBI from "jsbi";
 
-/**
- * Applies a function to a list of null/undefined values, unwrapping the null/undefined value or passing it through.
- */
-export const mapN = <T extends unknown[], U>(
-  fn: (
-    ...a: {
-      [K in keyof T]: NonNullable<T[K]>;
-    }
-  ) => U,
-  ...args: T
-): U | null | undefined => {
-  if (!args.every((arg) => arg !== undefined)) {
-    return undefined;
-  }
-  if (!args.every((arg) => arg !== null)) {
-    return null;
-  }
-  return fn(
-    ...(args as {
-      [K in keyof T]: NonNullable<T[K]>;
-    })
-  );
-};
+export * from "@saberhq/option-utils";
 
 /**
  * Blocks for the given duration.
