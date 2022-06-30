87bcd4dffe7277a068ada2b2d9f0830c1321d750
Ian Macalinao <ian@moving.wtf>
2022-06-30 13:43:23 -0500

    update aptos-api

--- packages/aptos-api/generate-api.sh
+++ packages/aptos-api/generate-api.sh
@@ -4,6 +4,6 @@ rm -fr fixtures/
 mkdir -p fixtures/
 curl https://raw.githubusercontent.com/aptos-labs/aptos-core/devnet/api/doc/openapi.yaml >fixtures/openapi.yaml
 
-yarn dlx swagger-typescript-api@latest -p fixtures/openapi.yaml \
-    --modular --single-http-client --extract-request-params --extract-request-body --responses \
+yarn swagger-typescript-api -p fixtures/openapi.yaml \
+    --modular --axios --single-http-client --extract-request-params --extract-request-body --responses \
     -o src/
