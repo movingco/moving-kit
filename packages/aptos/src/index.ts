10961d666eb6ae86d0524ca0ff992b55ca17bc42
Ian Macalinao <ian@moving.wtf>
2022-07-11 14:54:40 -0500

    adds Aptos faucet client

--- packages/aptos/src/index.ts
+++ packages/aptos/src/index.ts
@@ -2,6 +2,7 @@ export * from "./account.js";
 export * from "./api.js";
 export * from "./client.js";
 export * from "./constants.js";
+export * from "./faucet.js";
 export * from "./signingMessage.js";
 export * from "./testCoin.js";
 export * from "./transaction.js";
