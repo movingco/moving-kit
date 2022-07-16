b5ed1be3a9f5bf493ee4b49970b2b3fb157571c6
Ian Macalinao <ian@moving.wtf>
2022-07-16 14:31:29 -0500

    add explicit type exports to prelude serializers

--- packages/prelude/src/serializers.ts
+++ packages/prelude/src/serializers.ts
@@ -7,18 +7,18 @@ import {
 } from "@movingco/core";
 import type { HexStringArg } from "@movingco/idl";
 
-export const hexString = (arg: HexStringArg) => {
+export const hexString = (arg: HexStringArg): string => {
   const str = typeof arg === "string" ? arg : arg.hex();
   return HexString.ensure(str).hex();
 };
 
-export const u64 = (arg: BigintIsh) => {
+export const u64 = (arg: BigintIsh): string => {
   const bi = parseBigintIsh(arg);
   validateU64(bi);
   return bi.toString();
 };
 
-export const u128 = (arg: BigintIsh) => {
+export const u128 = (arg: BigintIsh): string => {
   const bi = parseBigintIsh(arg);
   validateU128(bi);
   return bi.toString();
